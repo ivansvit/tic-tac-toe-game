81dcef83919d48352aa65b488c1e5215bc03f085
IvanSvit <e#456yhn>
2021-04-20 15:33:54 +0200

    # Draw statement of the game # Add players name input # Play again function

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def game_field(board):
     print('-----------')
     print(f" {board['7']} | {board['8']} | {board['9']} ")
 
-
+# Player move function
 def player_move(board, user_input, player_letter):
     if board[user_input] == ' ':
         board[user_input] = player_letter
@@ -26,54 +26,73 @@ def player_move(board, user_input, player_letter):
 # Game logic
 def check_winner(board):
     if board['1'] == board['2'] == board['3'] != ' ':
-        print(f"{board['1']}, You win!!!")
         return False
     elif board['4'] == board['5'] == board['6'] != ' ':
-        print(f"{board['4']}, You win!!!")
         return False
     elif board['7'] == board['8'] == board['9'] != ' ':
-        print(f"{board['7']}, You win!!!")
         return False
     elif board['1'] == board['4'] == board['7'] != ' ':
-        print(f"{board['1']}, You win!!!")
         return False
     elif board['2'] == board['5'] == board['8'] != ' ':
-        print(f"{board['2']}, You win!!!")
         return False
     elif board['3'] == board['6'] == board['9'] != ' ':
-        print(f"{board['3']}, You win!!!")
         return False
     elif board['1'] == board['5'] == board['9'] != ' ':
-        print(f"{board['1']}, You win!!!")
         return False
     elif board['3'] == board['5'] == board['7'] != ' ':
-        print(f"{board['3']}, You win!!!")
         return False
     else:
         return True
 
 
-# Player input and mark the board
+# Game function. Player input and mark the board.
 def game(board):
-
+    step = 0
+    player_one_name = input("Player 1, please enter your name. ")
+    player_two_name = input("Player 2, please enter your name. ")
     game_field(board)
     while check_winner(board):
 
+        # Draw statement
+        if step == 4:
+            print("Draw!")
+            break
+
         # Player 1
         player_one_letter = 'X'
-        player_one_input = input(f"Player {player_one_letter}, choose a block to mark. "
+        player_one_input = input(f"{player_one_name}, choose a block to mark. "
                                  f"Enter a number of block from 1 to 9.\nTop-left block - 1, bottom-right - 9\n")
         player_move(board, player_one_input, player_one_letter)
+        if not check_winner(board):
+            print(f"{player_one_name} you win!!!")
 
+        # Player 2
         if check_winner(board):
-            # Player 2
             player_two_letter = 'O'
-            player_two_input = input(f"Player {player_two_letter}, choose a block to mark. "
+            player_two_input = input(f"{player_two_name}, choose a block to mark. "
                                      f"Enter a number of block from 1 to 9.\nTop-left block - 1, bottom-right - 9\n")
             player_move(board, player_two_input, player_two_letter)
+            if not check_winner(board):
+                print(f"{player_two_name} you win!!!")
+
+        step += 1
+
+
+# Play again function
+def restart_game():
+    player_answer = input("Do you want to play again? Enter Y/N. ").upper()
+    if player_answer == 'Y':
+        for key in ticTacToeBoard:
+            ticTacToeBoard[key] = ' '
+        game(ticTacToeBoard)
+        restart_game()
+    else:
+        print("See you later!")
 
 
 game(ticTacToeBoard)
+restart_game()
 
-# TODO Draw statement of the game
-# TODO Player X Winner double printing!
+# Draw statement of the game
+# Add players name input
+# Play again function
